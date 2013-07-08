c4352f3e68bab98fe1c2072ba828e3a1db521824
Kangie Chan <aries0420@gmail.com>
2013-07-08 00:40:37 +0000

    modified web.js

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+// app.get('/', function(request, response) {
+//   response.send('Hello World 2!');
+// });
+
+var fs = require('fs');
+
+fs.readFile('~/startupeng/bitstarter/index.html', function (err) {
+  if (err) throw err;
+  console.log(buf.toString("Hello World from index.html", 0, "utf-8"));
 });
 
 var port = process.env.PORT || 5000;
